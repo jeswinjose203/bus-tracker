f77c377bf18b144d7528f6ed8300da562ab5cbdc
Keerthiii098 <keerthibinu2507@gmail.com>
2023-03-05 21:50:36 +0530

    chittapan

--- first.js
+++ first.js
@@ -29,90 +29,51 @@ app.get('/',function(req,res){
         }
     });
 });
+
 var lat1,lon1;
+
 app.post('/bus_no_1/data', (req1, res1) => {
-    var { lat, lon } = req1.body;
-    // Handle the received data here
-    console.log(`Received data: lat=${lat}, lon=${lon}`);
-    lat1 = lat;
-    lon1 = lon; 
-    // Send a response to the client
-    res1.sendStatus(200);
-   res1.redirect('/bus_no_1');
-  });
-app.get('/bus_no_1',function(req,res)
-{
-    fs.readFile("index.html", function (error, pgResp){
-        
-        if (error) {
-            res.writeHead(404);
-            res.write('Contents you are looking are Not Found');
-        } else {
-            res.write(pgResp);
-            res.write(`
-            <h1>bus no 1</h1>
-            `);
-            if(lat1!=undefined && lon1!=undefined)
-            {
-                res.write('<h1>');
-                res.write(lat1.toString());
-                res.write('</h1>');
-
-                res.write('<h1>');
-                res.write(lon1.toString());
-                res.write('</h1>');
-
-
-                res.write(`
-                <!DOCTYPE html>
-<html>
-  <head>
-    <title>My Location</title>
-    <meta charset="utf-8" />
-    <style>
-      #myMap {
-        height: 400px;
-        width: 100%;
-      }
-    </style>
-    <script src="https://www.bing.com/api/maps/mapcontrol?key=AmhMfBZLCSDiPKsfakqFoNOIQAO2ot6WHmRfJOOByGBtg5zNzKwf6IN7zTl7DH2y&callback=loadMapScenario" async defer></script>
-    <script>
-      function loadMapScenario() {
-        navigator.geolocation.getCurrentPosition(
-          function (position) {
-            var latitude = ${lat1.toString()};
-            var longitude = ${lon1.toString()};
-            var map = new Microsoft.Maps.Map("#myMap", {
-              center: new Microsoft.Maps.Location(latitude, longitude),
-              zoom: 15,
-            });
-            var pushpin = new Microsoft.Maps.Pushpin(
-              map.getCenter(),
-              null
-            );
-            map.entities.push(pushpin);
-          },
-          function (error) {
-            console.log(error);
-          }
-        );
-      }
-    </script>
-  </head>
-  <body>
-    <div id="myMap"></div>
-  </body>
-</html>
-                `);
-            }
-              
-              
-            
-        res.end();
-
-        }
-    });
+  var { lat, lon } = req1.body;
+  // Handle the received data here
+  console.log(`Received data: lat=${lat}, lon=${lon}`);
+  lat1 = lat;
+  lon1 = lon; 
+  // Send a response to the client
+  res1.sendStatus(200);
 });
+app.get('/bus_no_1', function(req, res) {
+  fs.readFile("index.html", function (error, pgResp){
+    if (error) {
+      res.writeHead(404);
+      res.write('Contents you are looking are Not Found');
+      res.end();
+    } else {
+      fs.readFile("map.html", function (error, mapResp){
+        if (error) {
+          res.writeHead(404);
+          res.write('Contents you are looking are Not Found');
+          res.end();
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(pgResp);
+          res.write(`<h1>bus no 1</h1>`);
+          res.write(`<div id="bus-data"></div>`); // Add a div to hold the dynamic data
+          res.write(mapResp);
+          res.end();
+        }
+      });
+    }
+  });
+});
+
+app.get('/bus1',function(req,res){
+  res.send(`
+    <h1>lat : ${lat1}</h1>
+    <h1>lon : ${lon1}</h1>
+  `);
+});
+
+
 
 app.get('/bus_no_2',function(req,res)
 {
